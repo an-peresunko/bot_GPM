28c23da1e7c331030862698d0e6f3bf9b9d46038
an-peresunko <an.peresunko@icloud.com>
2023-07-29 16:48:41 +0300

    nothing new

--- bot_main.py
+++ bot_main.py
@@ -7,5 +7,14 @@ TOKEN = "6398064406:AAFqbo4-MolK2boLLkMDciZfrJKyaerHBZY"
 bot = Bot(TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+@bot.message_handler(commands=['start'])
+async def start_message(message):
+  bot.send_message(message.chat.id,"Твоё ФИО?")
+  # Пользователь вводит ФИО
+  # Бот спрашивает тип практики
+  # Бот спрашивает в каком отделе ты проходишь практику
+  # бот спрашивает у какого наставника ты закреплен
+  
+
 if __name__ == '__main__':
     executor.start_polling(dp)
